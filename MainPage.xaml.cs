73b187567c1e1c6e13b94168d17e5fa7dfc57411
Mukund Shah <mukundshah058@gmail.com>
2021-02-14 13:10:23 +0545

    Move to GitHub

---
+++ MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Timers;
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FullScreenClockApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        DispatcherTimer timer = new DispatcherTimer();
+        public MainPage()
+        {
+            this.InitializeComponent();
+            clock.Text = DateTime.Now.ToString("HH mm ss");
+            timer.Interval = TimeSpan.FromMilliseconds(1000);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            //SetTimer();
+            
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            clock.Text = DateTime.Now.ToString("HH mm ss");
+        }
+    
+
+    private void SetTimer()
+    {
+        Timer aTimer = new Timer(1000);
+        aTimer.Elapsed += OnTimedEvent;
+        aTimer.AutoReset = true;
+        aTimer.Enabled = true;
+    }
+
+    private void OnTimedEvent(object sender, ElapsedEventArgs e)
+    {
+    }
+
+
+}
+}
